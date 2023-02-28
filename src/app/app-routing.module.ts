da7fde0a685f1ab8c2d0577c83bccc261c92cff2
kevinmatias96 <kmati4596@gmail.com>
2023-02-28 08:59:42 -0300

    fin seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 
 const routes: Routes = [
-  { path: '', component:PortafolioComponent},
+  { path: 'home', component:PortafolioComponent},
   { path: 'about', component:AboutComponent},
   { path: 'item', component:ItemComponent},
   { path: '**', pathMatch: 'full', redirectTo:''}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
